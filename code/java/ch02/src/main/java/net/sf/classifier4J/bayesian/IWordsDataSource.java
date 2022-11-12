f4ba9cc3ca98e0a63798e9eeafd96b2b42aabddb
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-11-12 08:05:42 +0000

    Mi primer commit

--- code/java/ch02/src/main/java/net/sf/classifier4J/bayesian/IWordsDataSource.java
+++ code/java/ch02/src/main/java/net/sf/classifier4J/bayesian/IWordsDataSource.java
@@ -46,4 +46,45 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- * =============================================================
+ * ====================================================================
+ */
+
+package net.sf.classifier4J.bayesian;
+
+/**
+ * Interface used by BayesianClassifier to determine the probability of each word.
+ * 
+ * @author Nick Lothian 
+ * @author Peter Leschev
+ * 
+ */
+public interface IWordsDataSource {
+    /**
+     * @param word The word to calculate the probability of
+     * @return The word probability if the word exists, null otherwise;
+         *
+         * @throws WordsDataSourceException If there is a fatal problem. For 
+         *         example, the database is unavailable
+     */
+    public WordProbability getWordProbability(String word) throws WordsDataSourceException;
+
+    /**
+     * Add a matching word to the data source
+     * 
+     * @param word the word that matches
+         *
+         * @throws WordsDataSourceException If there is a fatal problem. For 
+         *         example, the database is unavailable
+     */
+    public void addMatch(String word) throws WordsDataSourceException;
+
+    /**
+     * Add a non-matching word to the data source
+     * 
+     * @param word the word that does not match
+         *
+         * @throws WordsDataSourceException If there is a fatal problem. For 
+         *         example, the database is unavailable
+     */
+    public void addNonMatch(String word) throws WordsDataSourceException;
+}
