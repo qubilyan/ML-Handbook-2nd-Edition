e8a0c1b4667bd5853eb6991d27ce489127a95ae4
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-11-13 11:19:26 +0000

    Prepare v1.0.0 release

--- code/java/ch02/src/main/java/net/sf/classifier4J/bayesian/SimpleWordsDataSource.java
+++ code/java/ch02/src/main/java/net/sf/classifier4J/bayesian/SimpleWordsDataSource.java
@@ -30,4 +30,85 @@
  *    http://sourceforge.net/users/nicklothian/.
  *
  * 5. Products derived from this software may not be called 
- *    "Classifier4J", nor may "Classifier4J" appear in their name
+ *    "Classifier4J", nor may "Classifier4J" appear in their names 
+ *    without prior written permission. For written permission, please 
+ *    contact http://sourceforge.net/users/nicklothian/.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ */
+
+package net.sf.classifier4J.bayesian;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Nick Lothian
+ * @author Peter Leschev
+ *  
+ */
+public class SimpleWordsDataSource implements IWordsDataSource, Serializable {
+
+    private Map words = new HashMap();
+
+    public void setWordProbability(WordProbability wp) {
+        words.put(wp.getWord(), wp);
+    }
+
+    /**
+     * @see net.sf.classifier4J.bayesian.IWordsDataSource#getWordProbability(String)
+     */
+    public WordProbability getWordProbability(String word) {
+        if (words.containsKey(word)) {
+            return (WordProbability) words.get(word);
+        } else {
+            return null;
+        }
+    }
+
+    public Collection getAll() {
+        return words.values();
+    }
+
+    /**
+     * @see net.sf.classifier4J.bayesian.IWordsDataSource#addMatch(String)
+     */
+    public void addMatch(String word) {
+        WordProbability wp = (WordProbability) words.get(word);
+        if (wp == null) {
+            wp = new WordProbability(word, 1, 0);
+        } else {
+            wp.setMatchingCount(wp.getMatchingCount() + 1);
+        }
+        setWordProbability(wp);
+    }
+
+    /**
+     * @see net.sf.classifier4J.bayesian.IWordsDataSource#addNonMatch(String)
+     */
+    public void addNonMatch(String word) {
+        WordProbability wp = (WordProbability) words.get(word);
+        if (wp == null) {
+            wp = new WordProbability(word, 0, 1);
+        } else {
+            wp.setNonMatchingCount(wp.getNonMatchingCount() + 1);
+        }
+        setWordProbability(wp);
+    }
+
+}
