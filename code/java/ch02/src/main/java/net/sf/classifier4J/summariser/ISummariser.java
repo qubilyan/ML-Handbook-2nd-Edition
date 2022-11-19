ddd4b8d901efd3dcb24b5ba08e58e77f34c1a8aa
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-11-19 11:24:01 +0000

    2004-08-02  Andrew Cagney  <cagney@gnu.org>

---
+++ code/java/ch02/src/main/java/net/sf/classifier4J/summariser/ISummariser.java
@@ -0,0 +1,9 @@
+/*
+ * ====================================================================
+ * 
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003-2005 Nick Lothian. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the follo
