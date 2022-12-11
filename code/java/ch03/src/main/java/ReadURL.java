c1d2ac9bb23f65f3a93e3cba01f693e7fa8ac7c4
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-12-11 10:02:25 +0000

    About

---
+++ code/java/ch03/src/main/java/ReadURL.java
@@ -0,0 +1,12 @@
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ReadURL {
+
+    public String r
