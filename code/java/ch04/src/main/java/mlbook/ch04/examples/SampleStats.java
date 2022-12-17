c2acf89397a340772b996777c991c0da952c2f9b
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-12-17 07:11:20 +0000

    version

--- code/java/ch04/src/main/java/mlbook/ch04/examples/SampleStats.java
+++ code/java/ch04/src/main/java/mlbook/ch04/examples/SampleStats.java
@@ -29,4 +29,89 @@ public class SampleStats {
         BufferedReader br = new BufferedReader(new FileReader(file));
         String s;
         while ((s = br.readLine()) != null) {
-            numList.add(Double.parseDouble(s))
+            numList.add(Double.parseDouble(s));
+        }
+        return numList;
+    }
+
+    // Exercise 2 - Finding Min and Max values of the collection.
+    public double getMinValue(List<Double> nums) {
+        return Collections.min(nums);
+    }
+
+    public double getMaxValue(List<Double> nums) {
+        return Collections.max(nums);
+    }
+
+    public int getSum(List<Integer> nums) {
+        int total = 0;
+        for (Integer i : nums) {
+            total += i.intValue();
+        }
+        return total;
+    }
+
+    public double getSumWithArrays(List<Double> nums) {
+        double[] pNumList = nums.stream()
+                             .mapToDouble(Double::doubleValue)
+                             .toArray();
+        return Arrays.stream(pNumList).sum();
+    }
+
+    public double getMean(List<Double> nums) {
+        double[] pNumList = nums.stream().mapToDouble(Double::doubleValue).toArray();
+        return StatUtils.mean(pNumList);
+    }
+
+    public double getHarmonicMean(List<Double> nums) {
+        double[] pNumList = nums.stream().mapToDouble(Double::doubleValue).toArray();
+        double reciprocolTotal = 0.0;
+        for(int i = 0 ; i < pNumList.length - 1 ; i++) {
+            reciprocolTotal += 1/pNumList[i];
+        }
+        double harmonicMean = pNumList.length/reciprocolTotal;
+        return harmonicMean;
+    }
+
+    public double getGeometricMean(List<Double> nums) {
+        double[] pNumList = nums.stream().mapToDouble(Double::doubleValue).toArray();
+        return StatUtils.geometricMean(pNumList);
+    }
+
+    public double[] getMode(List<Double> nums) {
+        double[] pNumList = nums.stream().mapToDouble(Double::doubleValue).toArray();
+        return StatUtils.mode(pNumList);
+    }
+
+    public double getMeadian(List<Double> nums) {
+        double[] pNumList = nums.stream().mapToDouble(Double::doubleValue).toArray();
+        DescriptiveStatistics ds = new DescriptiveStatistics();
+        for(int i = 0; i < pNumList.length -1 ; i++ ) {
+            ds.addValue(pNumList[i]);
+        }
+        return ds.getPercentile(50);
+    }
+
+    public double getRange(List<Double> nums) {
+        return (getMaxValue(nums) - getMinValue(nums));
+    }
+
+    public double getIQR(List<Double> nums) {
+        double[] pNumList = nums.stream().mapToDouble(Double::doubleValue).toArray();
+        DescriptiveStatistics ds = new DescriptiveStatistics();
+        for(int i = 0; i < pNumList.length -1 ; i++ ) {
+            ds.addValue(pNumList[i]);
+        }
+        return ds.getPercentile(75) - ds.getPercentile(25);
+    }
+
+    public double getVariance(List<Double> nums){
+        double[] pNumList = nums.stream().mapToDouble(Double::doubleValue).toArray();
+        SummaryStatistics ss = new SummaryStatistics();
+        for(int i = 0; i < pNumList.length -1 ; i++ ) {
+            ss.addValue(pNumList[i]);
+        }
+        return ss.getVariance();
+    }
+
+    public double getStandard
