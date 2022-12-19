46b935a08691c468dc7a3ba1c6dae20aaae4e5d0
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-12-19 08:55:29 +0000

    Few corrections

--- code/java/ch09/src/main/java/mlbook/ch09/ann/weka/BasicWekaMLP.java
+++ code/java/ch09/src/main/java/mlbook/ch09/ann/weka/BasicWekaMLP.java
@@ -19,4 +19,34 @@ public class BasicWekaMLP {
             training.setClassIndex(training.numAttributes() -1);
 
             MultilayerPerceptron mlp = new MultilayerPerceptron();
-            mlp.setOptions(Utils.splitOptions("-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H 4"
+            mlp.setOptions(Utils.splitOptions("-L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H 4"));
+
+
+            mlp.buildClassifier(training);
+
+            FileReader tr = new FileReader("/Users/jasebell/bookwork/mlbook2ndedition/data/ch09/weka/testdata.arff");
+            Instances testdata = new Instances(tr);
+            testdata.setClassIndex(testdata.numAttributes() -1);
+
+            Evaluation eval = new Evaluation(training);
+            eval.evaluateModel(mlp, testdata);
+            System.out.println(eval.toSummaryString("\nResults\n*******\n", true));
+
+            tr.close();
+            fr.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        BasicWekaMLP mlp = new BasicWekaMLP();
+    }
+
+}
+
