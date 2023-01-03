870fff6fdf9e16f911c770c81b84e8a02bfd0e90
qubilyan <151318276+qubilyan@users.noreply.github.com>
2023-01-03 14:45:20 +0000

    Run scan-local.sh on percona:...

--- code/shellscripts/csv2arff.sh
+++ code/shellscripts/csv2arff.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp /path/to/weka.jar weka.core.converters.CSVLoader kmeansdata.csv > kmeansdata.
+java -cp /path/to/weka.jar weka.core.converters.CSVLoader kmeansdata.csv > kmeansdata.arff
